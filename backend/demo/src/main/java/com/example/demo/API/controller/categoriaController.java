a94564e6bc90a460566721980dae353c745ad28d
mateoadann <mateoadan02@gmail.com>
2022-08-03 12:17:06 -0300

    Agrego categoria controller y entities

--- backend/demo/src/main/java/com/example/demo/API/controller/categoriaController.java
+++ backend/demo/src/main/java/com/example/demo/API/controller/categoriaController.java
@@ -1,4 +1,43 @@
 package com.example.demo.API.controller;
 
+import com.example.demo.API.persistence.entities.Categoria;
+import com.example.demo.API.service.categoriaService;
+import jdk.jfr.Category;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/categorias")
 public class categoriaController {
+
+    //Get
+    @GetMapping("/{id}")
+    public Categoria getCategoria(@PathVariable Long id) {
+        return categoriaService.leerCategoria(id);
+    }
+
+
+    //Post
+    @PostMapping
+    public ResponseEntity<?> crearCategoria(@RequestBody Categoria categoria){
+        categoriaService.crearCategoria(categoria);
+        return ResponseEntity.ok((HttpStatus.OK));
+    }
+
+    //Update
+    @PutMapping
+    public ResponseEntity<?> cambiarCategoria(@RequestBody Categoria categoria){
+        categoriaService.cambiarCategoria(categoria);
+        return ResponseEntity.ok(HttpStatus.ok);
+    }
+
+
+    //Delete
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminarCategoria(@PathVariable Long id){
+        categoriaService.eliminarCategoria(id);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
 }
--- backend/demo/src/main/java/com/example/demo/API/persistence/entities/categoria.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.API.persistence.entities;
-
-public class categoria {
-}
