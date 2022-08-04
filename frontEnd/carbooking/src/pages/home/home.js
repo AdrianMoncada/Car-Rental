10a59de74b22796d704811164d5fc3d61469d0e7
diana ramos <dianamil1941@gmail.com>
2022-08-04 12:43:27 -0500

    modificando arbol de archivos y carpeta de componantes

--- frontEnd/carbooking/src/pages/home/home.js
+++ frontEnd/carbooking/src/pages/home/home.js
@@ -1,26 +1,15 @@
 import React from 'react';
+import  Header  from '../../components/header';
+import  Footer  from '../../components/footer';
 
-import { Container,Header,ButtonSign,Footer,ContainerBtn,Icon,ContainerIcon } from './home.styles'
+import { Container } from './home.styles'
 
 
 const Home = () =>{
     return (
      <Container>
-         <Header>
-             <ContainerIcon>
-             <Icon src="/logo1.png" alt='digitlB'/>
-         <p>soy el eslogan</p>
-             </ContainerIcon>
-        
-         <ContainerBtn>
-         <ButtonSign>Crear cuenta</ButtonSign>
-         <ButtonSign>Iniciar sesion</ButtonSign>
-         </ContainerBtn>
-         </Header>
-         <Footer>
-             <p>©2021 Digital Booking</p>
-         </Footer>
-         
+        <Header/>
+        <Footer/>
 
      </Container>
     )
--- frontEnd/carbooking/src/pages/home/home.styles.js
+++ frontEnd/carbooking/src/pages/home/home.styles.js
@@ -4,8 +4,8 @@ export const Container = styled.div`
 width: 100%;
 height: 100%;
 min-height: 100vh;
-background: rgb(34,193,195);
-background: linear-gradient(0deg, rgba(44,55,195,1) 0%, rgba(232,228,98,1) 65%, rgba(221,253,45,1) 100%);
+background: #C4C4C4;
+
 display: flex;
 justify-content: center;
 text-align: center;
@@ -14,7 +14,7 @@ gap: 20px;
 flex-wrap: wrap;
 `;
 
-export const Header = styled.header`
+export const Headers = styled.header`
  display: flex;
  width: 100%;
  height: 80px;
@@ -24,15 +24,20 @@ export const Header = styled.header`
 `;
 
 export const ContainerIcon = styled.div`
-background: red;
+
 display: flex;
-justify-content:space-arround;
-color: white;
+justify-content:space-between;
+color: 191B1D;
 margin-top:30px;
 padding-right:20px;
-width:400px;
+width:210px;
 margin-left:40px;
 margin-top 10px;
+font-family: 'Quicksand';
+font-style: normal;
+font-weight: 300;
+font-size: 18px;
+line-height: 22px;
 `;
 
 export const Icon = styled.img`
@@ -59,7 +64,7 @@ export const ButtonSign = styled.button`
   
 `;
 
-export const Footer = styled.footer `
+export const Footers = styled.footer `
   background-color: #F0572D;
   color:white;
   padding: 24px;
