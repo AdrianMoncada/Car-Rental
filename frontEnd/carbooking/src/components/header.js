82f6a32d6bd4fdf200eaae007e2854d725162946
diana ramos <dianamil1941@gmail.com>
2022-08-04 19:03:29 -0500

    haciendo responsive la pagina del home

--- frontEnd/carbooking/src/components/header.js
+++ frontEnd/carbooking/src/components/header.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Headers,ButtonSign,ContainerBtn,Icon,ContainerIcon } from '../pages/home/home.styles'
+import { Headers,ButtonSign,ContainerBtn,
+        Icon,ContainerIcon,ContainerMenu,
+        Menu,Icon2,ContainerIcon2 } from '../pages/home/home.styles'
 
 const Header = () =>{
     return (
@@ -7,8 +9,16 @@ const Header = () =>{
       <Headers>
           <ContainerIcon>
              <Icon src="/logo1.png" alt='digitlB'/>
-             <p>modificar </p>
+            
+             <p>Tu camino, tus reglas </p>
           </ContainerIcon>
+          <ContainerIcon2>
+          <Icon2 src="/logo 5.png" alt='digitlB'/>
+
+          </ContainerIcon2>
+          <ContainerMenu>
+             <Menu src="/menu.png" alt='digitlB'/>
+          </ContainerMenu>
         
           <ContainerBtn>
              <ButtonSign>Crear cuenta</ButtonSign>
