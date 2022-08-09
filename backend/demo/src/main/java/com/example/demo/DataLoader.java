30e05f3b652b1f07b2649eb1a6df364c14ea2b2d
Adrián Moncada <adrian.ignaciomoncada@gmail.com>
2022-08-09 12:32:02 -0300

    database crea json estatico cuando inicia

---
+++ backend/demo/src/main/java/com/example/demo/DataLoader.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+import com.example.demo.API.persistence.entities.Category;
+import com.example.demo.API.persistence.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    public DataLoader(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public void run(ApplicationArguments args) {
+            categoryRepository.save(new Category("Economicos", "descripción", "https://images.pexels.com/photos/1007410/pexels-photo-1007410.jpeg?cs=srgb&dl=pexels-mike-b-1007410.jpg&fm=jpg"));
+            categoryRepository.save(new Category("Deportivos", "descripción", "https://images.pexels.com/photos/217326/pexels-photo-217326.jpeg?cs=srgb&dl=pexels-mike-b-217326.jpg&fm=jpg"));
+            categoryRepository.save(new Category("De Lujo", "descripción", "https://images.pexels.com/photos/1164778/pexels-photo-1164778.jpeg?cs=srgb&dl=pexels-christian-1164778.jpg&fm=jpg"));
+            categoryRepository.save(new Category("Minivan", "descripcion", "https://images.pexels.com/photos/11139392/pexels-photo-11139392.jpeg?cs=srgb&dl=pexels-hasan-gulec-11139392.jpg&fm=jpg"));
+    }
+}
