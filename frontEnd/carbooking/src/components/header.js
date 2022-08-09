521bdf65424539c75247629894bd5d05094f7984
diana ramos <dianamil1941@gmail.com>
2022-08-09 15:47:50 -0500

    se integro funcionalidad a los iconos del header que redireccionan al home

--- frontEnd/carbooking/src/components/header.js
+++ frontEnd/carbooking/src/components/header.js
@@ -1,34 +1,36 @@
 import React from 'react';
-import { useNavigate  } from 'react-router-dom';
-import { Headers,ButtonSign,ContainerBtn,
-        Icon,ContainerIcon,ContainerMenu,
-        Menu,Icon2,ContainerIcon2 } from '../pages/home/home.styles';
+import { useNavigate, Link } from 'react-router-dom';
+import {
+   Headers, ButtonSign, ContainerBtn,
+   Icon, ContainerIcon, ContainerMenu,
+   Menu, Icon2, ContainerIcon2
+} from '../pages/home/home.styles';
 
 
 const Header = () => {
    const navigate = useNavigate();
-    return (
+   return (
       <Headers>
-          <ContainerIcon>
-             <Icon src="/logo1.png" alt='digitlB'/>
-             <p>Tu camino, tus reglas </p>
-          </ContainerIcon>
-          <ContainerIcon2>
-          <Icon2 src="/logo 5.png" alt='digitlB'/>
+         <ContainerIcon>
+            <Link to="/home"><Icon src="/logo1.png" alt='digitlB' /></Link>
+            <p>Tu camino, tus reglas </p>
+         </ContainerIcon>
+         <ContainerIcon2>
+            <Link to="/home"><Icon2 src="/logo 5.png" alt='digitlB' /></Link>
 
-          </ContainerIcon2>
-          <ContainerMenu>
-             <Menu src="/menu.png" alt='digitlB'/>
-          </ContainerMenu>
-        
-          <ContainerBtn>
-               <ButtonSign onClick={() => navigate("signup")}>Crear cuenta</ButtonSign>
-               <ButtonSign onClick={() => navigate("signin")}>Iniciar sesion</ButtonSign>
-              
-          </ContainerBtn>
+         </ContainerIcon2>
+         <ContainerMenu>
+            <Menu src="/menu.png" alt='digitlB' />
+         </ContainerMenu>
+
+         <ContainerBtn>
+            <ButtonSign onClick={() => navigate("signup")}>Crear cuenta</ButtonSign>
+            <ButtonSign onClick={() => navigate("signin")}>Iniciar sesion</ButtonSign>
+
+         </ContainerBtn>
       </Headers>
 
-    )
+   )
 }
 
 export default Header
