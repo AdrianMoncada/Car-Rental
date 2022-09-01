30944eedcd9beddcd781d82d1cb8e9868f9d58b2
diana ramos <dianamil1941@gmail.com>
2022-09-01 13:42:25 -0500

    se modifico la hoja de stilos del archivo Booking.styles.js  para aplicar media query

--- frontEnd/carbooking/src/pages/Bookings/Booking.styled.js
+++ frontEnd/carbooking/src/pages/Bookings/Booking.styled.js
@@ -26,20 +26,34 @@ export const GridContainer = styled.div`
   grid-template-rows: 1.7fr 0.5fr 2.4fr 0.5fr 2.4fr 0.1fr 1fr;
   gap: 0px 0px;
   grid-auto-flow: row;
+  @media (max-width: 1224px) {
+    height:800px;
+  }
    
   
 `
 export const PersonalData = styled.div`
  grid-area: 1 / 1 / 2 / 2; 
  background:red;
+ @media (max-width: 1224px) {
+  grid-area: 1 / 1 / 2 / 4; 
+  background:orange;
+}
   `
 export const Calendar = styled.div`
  grid-area: 3 / 1 / 4 / 2; 
  background:blue;
+ @media (max-width: 1224px) {
+  grid-area: 3 / 1 / 4 / 4; 
+}
 `
 export const ReservedProduct = styled.div`
  grid-area: 1 / 3 / 6 / 4; 
  background:green;
+ @media (max-width: 1224px) {
+  grid-area: 6 / 1 / 7 / 4; 
+  height:200px;
+}
   `
 export const Policies = styled.div`
  grid-area: 7 / 1 / 8 / 4; 
@@ -49,6 +63,8 @@ export const Policies = styled.div`
 export const Schedule = styled.div`
  grid-area: 5 / 1 / 6 / 2; 
  background:pink;
-  
+ @media (max-width: 1224px) {
+  grid-area: 5 / 1 / 6 / 4; 
+}
 `
 
