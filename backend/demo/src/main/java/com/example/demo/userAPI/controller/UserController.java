c14efe411027380e4d1f1a00dab207fd1dc8b4bd
Adrián Moncada <adrian.ignaciomoncada@gmail.com>
2022-09-05 19:59:09 -0300

    agregando cors user

--- backend/demo/src/main/java/com/example/demo/userAPI/controller/UserController.java
+++ backend/demo/src/main/java/com/example/demo/userAPI/controller/UserController.java
@@ -16,6 +16,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+@CrossOrigin
 @RestController
 @RequestMapping("/users")
 public class UserController {
